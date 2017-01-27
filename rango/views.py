014d4c61cdd1b0c97c43c7eef6b6d0af9eefd04b
PiShing Koh <2222296k@student.gla.ac.uk>
2017-01-27 16:07:11 +0000

    Chapter 4 in progress

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>") 
+#Chapter 3
+#def index(request):
+#    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>") 
 	
+def index(request):
+	context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+	return render(request, 'rango/index.html', context = context_dict)
+
+#Chapter 3	
+#def about(request):
+#	return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+
 def about(request):
-	return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	some_dict = {'somemessage' : "This tutorial has been put together by Koh Pi Shing."}
+	return render(request, 'rango/about.html', context = some_dict)
